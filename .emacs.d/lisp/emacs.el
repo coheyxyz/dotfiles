fc59f99f07c7b169554ca23129dd550dbb040b92
Kohei Tsuruta <coheyxyz@gmail.com>
2017-02-10 14:35:25 -0800

    Fix yas expand

--- .emacs.d/lisp/emacs.el
+++ .emacs.d/lisp/emacs.el
@@ -192,7 +192,8 @@
 (use-package yasnippet
   :init (progn
           (yas-global-mode 1)
-          (setq-default ac-sources (append '(ac-source-yasnippet) ac-sources))))
+          (setq-default ac-sources (append '(ac-source-yasnippet) ac-sources))
+          (define-key yas-minor-mode-map (kbd "TAB") yas-maybe-expand)))
 
 
 ;;;
