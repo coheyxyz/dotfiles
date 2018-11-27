bb964c35c074559947e08803058a23968c94bd8c
Kohei Tsuruta <coheyxyz@gmail.com>
2018-11-27 11:22:20 -0800

    Use symlink

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/.bundle ~
+ln -sf ~/dotfiles/.gemrc ~
+ln -sf ~/dotfiles/.gitconfig ~
+ln -sf ~/dotfiles/.gitignore ~
+ln -sf ~/dotfiles/.hammerspoon ~
+ln -sf ~/dotfiles/.tmux.conf ~
+ln -sf ~/dotfiles/.tmux.mac.conf ~
+ln -sf ~/dotfiles/.zsh ~
+ln -sf ~/dotfiles/.zshrc ~
+
+ln -sf ~/dotfiles/.emacs.d/init.el ~/.emacs.d
+ln -sf ~/dotfiles/.emacs.d/lisp ~/.emacs.d
